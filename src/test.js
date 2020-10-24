48b64ddc543e659aa4313a21e40a767628a7ff89
GingkathFox <gingkathfox@gmail.com>
2020-10-24 16:15:54 -0400

    555 on the first try, i think im god yall

--- src/test.js
+++ src/test.js
@@ -1,3 +1,4 @@
+// broken seven segment display test
 /*const sevenSegmentDisplay = require('../lib/libsevensegmentdisplay')
 const one = [0, 1, 1, 0, 0, 0, 0]
 const two = [1, 1, 0, 1, 1, 0, 1]
@@ -16,7 +17,8 @@ let ssd2 = new sevenSegmentDisplay(two, true, ssd1.rawInput)
 console.log(ssd2.raw.join('\n'))
 console.log(ssd2.value)*/
 
-
-for (let i = 0; i < 5; i++) {
-
-}
+const ic555 = require('../lib/lib555')
+const triple5 = new ic555(1000)
+triple5.on('pulse', () => {
+    console.log('555 Pulse')
+})
